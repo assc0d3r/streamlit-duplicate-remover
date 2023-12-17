d82ace21c9109c0d99e3f34b9ab30a30b3b90f70
c0d3r <strug1538@flyymail.com>
2023-12-17 22:24:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ else:
         api_id=API_ID,
         api_hash=API_HASH,
         bot_token=BOT_TOKEN,
-        chat_list=chat_list,
-    )
+            )
     logger.info("Bot started using Bot Token")
  
 # calculate file size
