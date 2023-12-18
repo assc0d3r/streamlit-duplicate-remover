3c3f786a71cd3fa63f4638bb7c3dc8370b00f199
c0d3r <strug1538@flyymail.com>
2023-12-18 00:38:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ async def init():
         
      return False
     
-client = TelegramClient('my_bot', api_id, api_hash)
+client = TelegramClient('my_bot', API_ID, API_HASH)
 with client:
      print("Initialize check for duplicate files")
      client.loop.run_until_complete(init())
