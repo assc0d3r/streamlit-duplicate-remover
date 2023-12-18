ea94fc157dcc5f98e4b1c4ea8ff5d82093d95ce1
c0d3r <strug1538@flyymail.com>
2023-12-18 18:39:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -130,5 +130,5 @@ with client:
      print("Initialize check for duplicate files")
      asyncio.get_event_loop().run_until_complete(init())
      print("Start listening for new messages:")
-     client.add_event_handler(handler()
+     client.add_event_handler(handler)
      client.run_until_disconnected()
