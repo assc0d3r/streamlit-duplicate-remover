1b94f4c94b75893253e3098ab2ffc0d509fafada
c0d3r <strug1538@flyymail.com>
2023-12-19 18:18:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -127,11 +127,13 @@ async def init():
      return False
                  #await client.disconnected     
 client = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-with client:
-     print("Initialize check for duplicate files")
-     #asyncio.run(init())
-     #asyncio.get_event_loop().run_until_complete(init())
-     client.loop.run_until_complete(init())
+try:
+    with client:
+    print("Initialize check for duplicate files")
+    #asyncio.run(init())
+    #asyncio.get_event_loop().run_until_complete(init())
+    client.run_until_complete(init())
+finally:
      print("Start listening for new messages:")
      client.add_event_handler(handler)
      #client.add_event_handler(handler)
