0bf48748b931dcfce20514a58feb24d9e28d3001
c0d3r <strug1538@flyymail.com>
2023-12-19 01:21:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -128,11 +128,11 @@ async def init():
 
 client = TelegramClient(StringSession(SESSION), APP_ID, API_HASH, sequential_updates=True)
 with client:
-     print("Initialize check for duplicate files")
+     #print("Initialize check for duplicate files")
      #asyncio.get_event_loop().run_until_complete(init())
-     client.loop.run_until_complete(init())
+     #client.loop.run_until_complete(init())
      print("Start listening for new messages:")
-     client.add_event_handler(handler, events.NewMessage)
+     client.add_event_handler(handler)
      # client.add_event_handler(handler)
      client.run_until_disconnected()
      
