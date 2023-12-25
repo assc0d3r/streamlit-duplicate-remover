d4cf399ea7438ef3ecd5f5862579df571c90d224
c0d3r <strug1538@flyymail.com>
2023-12-25 17:52:46 +0000

    Update flask.py

--- flask.py
+++ flask.py
@@ -8,9 +8,9 @@ from telethon.sessions import StringSession
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#@app.route('/')
+#def hello_world():
+    #return 'Hello, World!'
 
 logging.basicConfig(
     level=logging.DEBUG,
