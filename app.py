6a0ade0fc7be4581e30145e2b8f8dbbf96ab0862
c0d3r <strug1538@flyymail.com>
2023-12-25 21:43:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'tglf'
+    return 'CHECKING AND REMOVING DUPLICATES FROM THE CHANNEL IS ACTIVATED AND IS PROGRESSING WELL....'
 
 
 if __name__ == "__main__":
