f4a59a93cedc921e420cd8c0aceb1ab32dcee1d8
c0d3r <strug1538@flyymail.com>
2023-12-25 17:49:13 +0000

    Update flask.py

--- flask.py
+++ flask.py
@@ -9,8 +9,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-#def hello_world():
-    #return 'Hello, World!'
+def hello_world():
+    return 'Hello, World!'
 
 logging.basicConfig(
     level=logging.DEBUG,
